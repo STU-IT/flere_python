f8836eac98f811823babe91bbc74970a8dbc5d77
Søren Magnusson <soren@magnussons.dk>
2017-09-25 09:22:25 +0200

    Skabelon

--- taxi-fare/taxi_fare.py
+++ taxi-fare/taxi_fare.py
@@ -1,12 +1,5 @@
 def taxi_fare(km):
-    grundpris = 4.00
-    kmpris    = 0.25 / 0.140    # 25 cent pr 140 meter
-
-    if km > 0:
-        turpris = grundpris + km * kmpris
-    else:
-        turpris = grundpris
-    return turpris
+   pass
 
 print(taxi_fare(1))
 print(taxi_fare(3.36))
